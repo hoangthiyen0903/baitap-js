7a04d11ca437d9312fe2b73ad6beb9ef7f4631f4
DaiDV <dangvandai1992@gmail.com>
2022-10-20 23:58:40 +0700

    js caculator

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
-	$("p").click(function(){
-		$(this).hide();
-	});
-}
+    $('.box-result').attr('disabled', true);
+
+    $('.number, .operation, .dot').click(function(){
+        //debugger;
+        var number = $(this).html();
+        var input = $('.box-calculation').val();
+        if(input != ""){
+            $('.box-calculation').val(input + number);
+        }else{
+            $('.box-calculation').val(number);
+        }
+    });
+
+    $('.refresh').click(function(){
+        $('.box-calculation').val('')
+    });
+
+
+    $('.del').click(function(){
+        var input = $('.box-calculation').val();
+        $('.box-calculation').val(input.slice(0, -1));
+    });
+    $('.equal').click(function(){
+        var input = $('.box-calculation').val();
+        $('.box-calculation').val(eval(input));
+    });
+});
